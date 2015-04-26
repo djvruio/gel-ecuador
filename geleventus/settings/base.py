022a0a6a90ad0b661b571f09171317f9da140cb8
djvruio <djvruio@gmail.com>
2015-04-26 15:39:56 -0500

    geleventus: postgres configured.

--- geleventus/settings/base.py
+++ geleventus/settings/base.py
@@ -17,15 +17,24 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+DJANGO_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+    )
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-)
+LOCAL_APPS = (
+        'apps.events',
+    )
+
+THIRD_PARTY_APPS = (
+
+    )
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
--- geleventus/settings/local.py
+++ geleventus/settings/local.py
@@ -5,8 +5,12 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'glaldb',
+        'USER' : 'glaluser',
+        'PASSWORD': 'glal#2015',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
