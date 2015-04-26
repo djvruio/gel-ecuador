92237e4745732b46eacb985705991273539e6d83
djvruio <djvruio@gmail.com>
2015-04-26 16:21:58 -0500

    geleventus: events models added.

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+
+class TimeStampModel(models.Model):
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		abstract = True
+
+class Category(models.Model):
+	"""Categoria del evento"""
+	name = models.CharField(max_length=50)
+
+	def save(self, *args, **kwargs):
+		if not self.id:
+			self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
+		
+	def __unicode__(self):
+		return self.name
+
+
+class Event(TimeStampModel):
+	name = models.CharField(max_length=200, unique=True)
+	slug = models.SlugField(editable=False)
+	summary = models.TextField(max_length=255)
+	content = models.TextField()
+	category = models.ForeignKey(Category)
+	place = models.CharField(max_length=50)
+	start = models.DateTimeField()
+	finish = models.DateTimeField()
+	image = models.ImageField(upload_to = 'events') #falta algo
+	is_free = models.BooleanField(default=True)
+	amount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+	views = models.PositiveIntegerField(default=0)
+	organizer = models.ForeignKey(User) #falta algo
+
+	def save(self, *args, **kwargs):
+		if not self.id:
+			self.slug = slugify(self.name)
+		super(Event, self).save(*args, **kwargs)
+
+	def __unicode__(self):
+		return self.name
+
+class Assistant(TimeStampModel):
+	assistant = models.ForeignKey(User) #falta algo
+	event = models.ManyToManyField(Event)
+	attended = models.BooleanField(default=False)
+	has_paid = models.BooleanField(default=False)
+
+	def __unicode__(self):
+		return "%s %s" % (self.assistant.username, self.event.name)
+
+
+class Comments(TimeStampModel):
+	user = models.ForeignKey(User)
+	event = models.ForeignKey(Event)
+	content = models.TextField()
+
+	def __unicode__(self):
+		return "%s %s" % (self.user.username, self.event.name)
+
