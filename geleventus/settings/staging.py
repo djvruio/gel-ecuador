a1f5e340ab673f30eaa145ee8755a3d65118e330
djvruio <djvruio@gmail.com>
2015-04-26 22:31:19 -0500

    geleventus:setting staging.txt

--- geleventus/settings/staging.py
+++ geleventus/settings/staging.py
@@ -6,10 +6,10 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'glaldb',
-        'USER' : 'glaluser',
-        'PASSWORD': 'glal#2015',
-        'HOST': 'localhost',
+        'NAME': 'd201iou2379hvu',
+        'USER' : 'iujgxkrtiffacz',
+        'PASSWORD': 'jGwx2eiinC5N5aDEoc7kNuRjgY',
+        'HOST': 'ec2-23-21-140-156.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
